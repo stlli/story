c5acf8667310e3453866480472d972425af75365
Philip Lin <jobstlli@gmail.com>
2025-09-14 13:18:24 -0700

    add script comment

--- scripts/downloadPokemonImages.js
+++ scripts/downloadPokemonImages.js
@@ -1,18 +1,37 @@
+/**
+ * This script downloads the images of specified Pokemon from the Pokemon
+ * DB website and saves them in the specified directory.
+ *
+ * It uses the pokemonList array to determine which Pokemon to download.
+ * Each Pokemon's name is used to construct the URL for its image.
+ * The images are saved in the specified directory, with the same name as the Pokemon.
+ *
+ * The script first checks if the image directory exists, and creates it if it doesn't.
+ * Then it uses the downloadImage function to download each Pokemon's image.
+ * The downloadImage function creates a writable stream to save the image,
+ * and uses the https.get function to download the image from the specified URL.
+ * Once the image is downloaded, the stream is closed and the promise is resolved.
+ *
+ *
+ * Usage:
+ *   node scripts/downloadPokemonImages.js
+ *
+ * This script can be run from the command line using the following command:
+ *
+ *   node scripts/downloadPokemonImages.js
+ *
+ * When the script is run, it will download the images of the specified Pokemon and save them in the specified directory.
+ *
+ * If any error occurs during the download process, the promise is rejected with the error.
+ */
+
 const fs = require('fs');
 const https = require('https');
 const path = require('path');
 
 // List of Pokemon names from entities_pm.js
-const pokemonList = [
-    'pikachu',
-    'charizard',
-    'mewtwo',
-    'bulbasaur',
-    'squirtle',
-    'dragonite',
-    'sylveon',
-    'gardevoir'
-];
+const entitiesPm = require('../data/entities_pm.json');
+const pokemonList = Object.values(entitiesPm).map(entity => entity.character.name);
 
 // Create images directory if it doesn't exist
 const imageDir = path.join(__dirname, '../public/images/entities');
